b26256a387d71cb52ab026deb2d851402377da21
Aster <galaster@foxmail.com>
2021-12-22 01:38:34 +0800

    🔧 Basic framework of the page

--- projects/doki-core/src/widgets/mod.rs
+++ projects/doki-core/src/widgets/mod.rs
@@ -1,7 +1,52 @@
 use sycamore::prelude::*;
-use sycamore::builder::html::{nav, header};
+use sycamore::builder::html::*;
 
-#[component(DocumentHeader<G>)]
-fn doc_header(value: ReadSignal<i32>) -> View<G> {
+
+#[component(DocumentRoot < G >)]
+fn builder(is_night: ReadSignal<bool>) -> View<G> {
+    body()
+        .dyn_class("night", is_night)
+        // .dyn_class("light", !is_night)
+        .child(DocumentHeader::create_component(()))
+        .child(DocumentSidebar::create_component(()))
+        .child(DocumentContext::create_component(()))
+        .build()
+}
+
+#[component(DocumentHeader < G >)]
+fn builder() -> View<G> {
     header().build()
 }
+
+#[component(DocumentSidebar < G >)]
+fn builder() -> View<G> {
+    aside().build()
+}
+
+#[component(DocumentContext < G >)]
+fn builder() -> View<G> {
+    main()
+        .child(DocumentArticle::create_component(()))
+        .child(DocumentFooter::create_component(()))
+        .child(DocumentNav::create_component(()))
+        .build()
+}
+
+
+#[component(DocumentArticle < G >)]
+fn builder() -> View<G> {
+    article().build()
+}
+
+
+#[component(DocumentFooter < G >)]
+fn builder() -> View<G> {
+    article().build()
+}
+
+#[component(DocumentNav < G >)]
+fn builder() -> View<G> {
+    nav().build()
+}
+
+
