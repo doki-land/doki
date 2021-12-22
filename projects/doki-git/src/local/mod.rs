44f847e1cb5a38ef93f94bd1cc62d08fa0f3b040
Aster <galaster@foxmail.com>
2021-12-22 12:07:55 +0800

    🔮 Support count commit lines

--- projects/doki-git/src/local/mod.rs
+++ projects/doki-git/src/local/mod.rs
@@ -1,15 +1,48 @@
 use std::path::Path;
-use git2::Repository;
+use std::time::{Duration, UNIX_EPOCH};
+use chrono::NaiveDateTime;
+use git2::{BlameHunk, Oid, Repository};
+use project_root::get_project_root;
 
 
 #[test]
 fn open() {
-    let repo = match Repository::open("../../") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
+    let root = get_project_root().unwrap();
+    ;
+    let repo = Repository::open(root).unwrap();
     let file = repo.blame_file(&Path::new("Cargo.toml"), None).unwrap();
+
+    repo.reflog("a");
     for i in file.iter() {
-        println!("{:#?}", i.orig_signature().when())
+        //i.final_signature()
+        println!("{:?}", i.orig_commit_id());
+        println!("{:?}", i.lines_in_hunk());
+        let sig = i.orig_signature();
+        println!("{:?}", sig.name());
+        println!("{:?}", sig.email());
+        println!("{:?}", NaiveDateTime::from_timestamp(sig.when().seconds(), 0));
+        println!();
+    }
+}
+
+#[derive(Debug)]
+pub struct FileCommitItem {
+    id: Oid,
+    lines: usize,
+    name: Option<String>,
+    email: Option<String>,
+    time: NaiveDateTime,
+}
+
+impl From<BlameHunk<'_>> for FileCommitItem {
+    fn from(blame: BlameHunk) -> Self {
+        let sig = blame.orig_signature();
+        Self {
+            id: blame.orig_commit_id(),
+            lines: blame.lines_in_hunk(),
+            name: sig.name().map(|s| s.to_string()),
+            email: sig.email().map(|s| s.to_string()),
+            time: NaiveDateTime::from_timestamp(sig.when().seconds(), 0),
+        }
     }
 }
