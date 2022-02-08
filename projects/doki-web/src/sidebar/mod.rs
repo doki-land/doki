6e114fa6637c08ebb6e6b431ad9ad37f0dce8a52
Aster <galaster@foxmail.com>
2022-02-09 00:52:32 +0800

    🔮 Support SideNavGroupItemSimple

--- projects/doki-web/src/sidebar/mod.rs
+++ projects/doki-web/src/sidebar/mod.rs
@@ -18,26 +18,47 @@ pub fn SideNav(cx: Scope) -> Element {
 pub struct SideNavGroupData {
     #[props(optional)]
     title: Option<String>,
-    items: Vec<String>
+    items: Vec<String>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum SideNavGroupItemIcon {
+    Numeric,
+    Icon(String),
+}
+
+#[derive(Props, PartialEq)]
+pub struct SideNavGroupItemSimple {
+    #[props(optional)]
+    icon: Option<SideNavGroupItemIcon>,
+    name: String,
+    link: String,
+}
+
+#[derive(Props, PartialEq)]
+pub struct SideNavGroupItemList {
+    button: String,
+    #[props(optional)]
+    can_close: Option<bool>,
+    #[props(optional)]
+    start_close: Option<bool>,
+    list: Vec<SideNavGroupItemList>,
 }
 
 pub enum SideNavGroupItem {
-
+    Simple(SideNavGroupItemSimple),
+    List(SideNavGroupItemList),
 }
-
 
 impl Default for SideNavGroupData {
     fn default() -> Self {
-        Self {
-            title: None,
-            items: vec![]
-        }
+        Self { title: None, items: vec![] }
     }
 }
 
 impl SideNavGroupData {
-    pub fn render_title<'a, 'b>(&'a self, cx: &'b Scope<'a, Self>) -> Option<LazyNodes<'a,'b>> {
-        cx.props.title.as_ref().map(|title| {
+    pub fn render_title<'a>(&'a self) -> Option<LazyNodes<'a, '_>> {
+        self.title.as_ref().map(|title| {
             rsx! {
                 li {
                     class: "flex flex-row",
@@ -48,8 +69,47 @@ impl SideNavGroupData {
     }
 }
 
+impl SideNavGroupItemSimple {
+    pub fn render(&self) -> LazyNodes {
+        let icon = self.icon.as_ref().map(|icon| icon.render());
+        let text = self.name.as_str();
+        let link = self.link.as_str();
+        rsx! {
+            li {
+                class: "flex flex-row",
+                icon
+                a {
+                    href: "{link}",
+                    "{text}"
+                }
+            }
+        }
+    }
+}
+
+impl SideNavGroupItemIcon {
+    pub fn render(&self) -> LazyNodes {
+        match self {
+            Self::Numeric => {
+                rsx! {
+                    span {
+                        "1."
+                    }
+                }
+            }
+            Self::Icon(_) => {
+                rsx! {
+                    svg {
+                    class: "w-1 h-1",
+                }
+                }
+            }
+        }
+    }
+}
+
 pub fn SideNavGroup(cx: Scope<SideNavGroupData>) -> Element {
-    let title = cx.props.render_title(&cx);
+    let title = cx.props.render_title();
     cx.render(rsx! {
         ul {
                 title
