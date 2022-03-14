4d21bc3d1b6af6ad66fb97452b354444a28ec4cb
Aster <galaster@foxmail.com>
2022-03-14 16:10:30 +0800

    Upgrade dependencies

--- projects/doki-error/src/lib.rs
+++ projects/doki-error/src/lib.rs
@@ -8,12 +8,3 @@ mod error_3rd;
 pub use self::error::{DokiError, DokiErrorKind, MaybeRanged, Result};
 pub use url::Url;
 pub use yggdrasil_shared::DiagnosticLevel;
-
-#[cfg(feature = "git2")]
-pub extern crate git2;
-#[cfg(feature = "globset")]
-pub extern crate globset;
-#[cfg(feature = "nom")]
-pub extern crate nom;
-#[cfg(feature = "num")]
-pub extern crate num;
